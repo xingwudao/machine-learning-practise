39583ba15d75119338243a2253edacffabb71988
xingwudao <kaijiangchen@gmail.com>
2014-07-24 22:44:47 +0800

    debug mcmc.cpp

--- mcmc/mcmc.cpp
+++ mcmc/mcmc.cpp
@@ -7,6 +7,7 @@
 #include <cmath>
 using namespace std;
 #include <ctime>
+#include <vector>
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_real_distribution.hpp>
 #include <boost/random/variate_generator.hpp>
@@ -16,9 +17,10 @@ double StdNormal(double z)
     return exp(-z * z / 2.0) * sqrt(2 * 3.1416);
 }
 
+uint32_t seed = time(0);
 double Uniform(double min, double max)
 {
-    boost::mt19937 randomNumbergenerator( time( 0 ) );
+    boost::mt19937 randomNumbergenerator(seed++);
     boost::random::uniform_real_distribution< > uniformDistribution(min, max);
     boost::variate_generator< boost::mt19937&, boost::random::uniform_real_distribution < > >
         generateRandomNumbers( randomNumbergenerator, uniformDistribution );
@@ -43,7 +45,7 @@ int main(int c, char** v)
     for(uint32_t i = 0; i < states_num; i++)
     {
         double candidate = x + states[i];
-        double accept_prob = StdNorm(candidate)/StdNormal(x);
+        double accept_prob = StdNormal(candidate)/StdNormal(x);
         accept_prob = 1 > accept_prob ? accept_prob : 1;
         double u = Uniform(0, 1);
         if(u < accept_prob)
